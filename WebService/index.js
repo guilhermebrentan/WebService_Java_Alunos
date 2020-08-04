b0802486f2d7e76f88347cd17afc49deeb775104
GuidoBrentan <brentan996@gmail.com>
2020-08-04 20:14:41 -0300

    Executar funcional

--- WebService/index.js
+++ WebService/index.js
@@ -20,3 +20,29 @@ app.use("/", router);
 
 app.listen(port);
 console.log("O servidor está online!");
+
+// rotas da aplicação
+
+router.get('/executar/:RA?/:codDisciplina?/:nota?/:frequencia?', (req, res) => {
+    var RA = parseInt(req.params.RA);
+    var codDisciplina = parseInt(req.params.codDisciplina);
+    var nota = parseFloat(req.params.nota);
+    var frequencia = parseFloat(req.params.frequencia);
+
+    var condicao = "where RA = " + RA + " and Cod = " + codDisciplina;
+    var comandoSql = 'delete from Matriculas ' + condicao;
+
+    execSQLQuery(comandoSql, res);
+
+    comandoSql ='insert into Resultados values(' + RA + "," + codDisciplina + "," + nota + "," + frequencia + ')';
+
+    execSQLQuery(comandoSql, res);
+});
+
+//métodos adicionais
+function execSQLQuery(sqlQry, res){
+    global.conn.request()
+    .query(sqlQry)
+    .then(result => res.json(result.recordset))
+    .catch(err => res.json(err));
+}
